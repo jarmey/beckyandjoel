7be13f599efff6fab8b5ed1331cb9d4b639c8e0d
Joel Armey <joel.armey@realpage.com>
2019-01-18 16:17:35 -0600

    Add html to build

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,19 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var babel = require('gulp-babel');
 
-gulp.task('build-sass', function(){
-    return gulp.src('src/scss/beckyandjoel.scss')
-        .pipe(sass()) // Using gulp-sass
-        .pipe(gulp.dest('dist/css'))
+gulp.task('build-css', function(){
+  return gulp.src('src/scss/beckyandjoel.scss')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('dist/css'))
 });
 
 gulp.task('build-scripts', function () {
   return gulp.src('src/js/app.js')
     .pipe(babel())
     .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('build-html', function () {
+  return gulp.src('src/html/index.html')
+    .pipe(gulp.dest('dist/'));
 });

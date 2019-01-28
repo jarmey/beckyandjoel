c58043904389e192539d1e673723bdeb145690b5
Joel Armey <joel.armey@realpage.com>
2019-01-28 08:04:43 -0600

    Add bootstrap; Add images; Add ipsum text; Add minify to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var babel = require('gulp-babel');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var clean = require('gulp-clean');
+
+gulp.task('clean', function () {
+  return gulp.src('dist/*/*.*', {read: false})
+    .pipe(clean());
+});
 
 gulp.task('build-css', function(){
-  return gulp.src('src/styles/*.*')
+  return gulp.src(['src/styles/*.*', 'node_modules/bootstrap/dist/css/bootstrap.min.css'])
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('dist/css'))
 });
@@ -13,7 +20,13 @@ gulp.task('build-scripts', function () {
   return gulp.src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js', 'src/js/*.*'])
     .pipe(babel())
     .pipe(concat('app.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('build-images', function () {
+  return gulp.src('src/images/*.*')
+    .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('build-html', function () {
@@ -21,4 +34,4 @@ gulp.task('build-html', function () {
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('default', gulp.series('build-css', 'build-scripts', 'build-html'));
+gulp.task('default', gulp.series('clean', 'build-css', 'build-scripts', 'build-images', 'build-html'));
